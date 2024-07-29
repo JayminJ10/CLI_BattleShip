1e48ef38d9fd914ba5e6252d2593cf74d8b33bc3
JayminJ10 <jayminjhaveri10@gmail.com>
2024-07-29 19:03:35 -0400

    Oops pt2

--- src/BSServerClient.c
+++ src/BSServerClient.c
@@ -149,7 +149,8 @@ void client_game_loop(int port, Tile **board) {
         switch(received_packet.type) {
             case PACKET_COORDINATE:
                 // Update board first
-                u8 hit = 0;
+                u8 hit;
+                hit = (unsigned short) 0;
                 u8 x = received_packet.packet.data.indi;
                 u8 y = received_packet.packet.data.indj;
                 if (board[x][y].sym == '#') {
